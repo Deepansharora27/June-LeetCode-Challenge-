39d4f698014a6bb6a6cff403cf42033cebb89b6c
Deepanshu <deepansharora27@gmail.com>
2020-06-24 09:37:26 +0530

    Problem 12

---
+++ N-aryTreePostOrderTraversal.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    void postOrderHelper(Node *root, vector<int> &res)
+    {
+
+        for (auto temp : root->children)
+        {
+            if (temp)
+            {
+                postOrderHelper(temp, res);
+            }
+        }
+
+        res.push_back(root->val);
+    }
+
+    vector<int> postorder(Node *root)
+    {
+        vector<int> res;
+
+        if (!root)
+        {
+            return res;
+        }
+
+        postOrderHelper(root, res);
+        return res;
+    }
+};
